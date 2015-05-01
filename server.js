02c5f0b25c04889eee1c80a858b75719980a4524
Joe Emprayil <joe.emprayil@hp.com>
2015-05-01 11:28:14 -0500

    Changes to deploy to openshift

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ io.on('connection', function(socket){
   });
 });
 
-http.listen(8000, function (){
-	console.log('chat server listening on 8000');
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
+server.listen(server_port, server_ip_address, function(){
+  console.log("Listening on " + server_ip_address + ", server_port " + server_port)
 });
